4b4330cac448e73c51bcc826cc93c9929408123f
Koby <devbogo@gmail.com>
2019-05-06 10:51:54 +0900

    Merge branch 'master' of /Users/koby/dev/IdeaProjects/web with conflicts.

---
+++ src/main/java/com/dbogo/web/controller/CommentController.java
@@ -0,0 +1,15 @@
+package com.dbogo.web.controller;
+
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * project
+ */
+@Controller
+@RequestMapping("")
+@Log
+public class CommentController {
+
+}
