6ba8ae5c6a2fd11fe585640abe005afba0b5e379
Koby <devbogo@gmail.com>
2019-05-12 22:51:07 +0900

    comment

--- src/main/java/com/dbogo/web/controller/CommentController.java
+++ src/main/java/com/dbogo/web/controller/CommentController.java
@@ -1,15 +1,48 @@
 package com.dbogo.web.controller;
 
+import com.dbogo.web.domain.Comment;
+import com.dbogo.web.domain.FreeBoard;
+import com.dbogo.web.persistence.CommentRepository;
 import lombok.extern.java.Log;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-/**
- * project
- */
-@Controller
-@RequestMapping("")
+import javax.transaction.Transactional;
+import java.util.List;
+
+@RestController
+@Transactional
 @Log
+@RequestMapping("/comment/*")
 public class CommentController {
+
+    @Autowired
+    private CommentRepository commentRepo;
+
+    @PostMapping("/{bno}")
+    public ResponseEntity<List<Comment>> add(
+            @PathVariable("bno") Long bno,
+            @RequestBody Comment comment) {
+
+
+        FreeBoard board = new FreeBoard();
+        board.setSeq(bno);
+        commentRepo.save(comment);
+
+
+        return new ResponseEntity<>(getListByFreeBoard(board), HttpStatus.CREATED);
+
+    }
+
+    private List<Comment> getListByFreeBoard(FreeBoard board) throws RuntimeException {
+        log.info("getListByFreeBoard .... " + board);
+
+        return commentRepo.getCommentOfFreeBord(board);
+    }
+
+
+
 
 }
--- src/main/java/com/dbogo/web/persistence/CommentRepository.java
+++ src/main/java/com/dbogo/web/persistence/CommentRepository.java
@@ -1,8 +1,21 @@
 package com.dbogo.web.persistence;
 
 import com.dbogo.web.domain.Comment;
+import com.dbogo.web.domain.FreeBoard;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends CrudRepository<Comment, Long> {
+
+
+    @Query("SELECT r FROM Comment r WHERE r.board = ?1 " + " AND r.seq > 0 ORDER BY r.seq ASC")
+    public List<Comment> getCommentOfFreeBord(FreeBoard board);
+
+
+
+
+
 
 }
